2f445deddca14aee4a2c884e837776b81e7ba418
Bibin T <bibint2918@gmail.com>
2025-06-02 01:31:10 +0530

    changed api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,8 @@ import userRoutes from './routes/userRoutes.js'
 dotenv.config();
 const app = express();
 
-app.use(cors({origin:'http://localhost:5173',credentials:true}));
+// app.use(cors({origin:'http://localhost:5173',credentials:true}));
+app.use(cors({origin:'https://article-feeds-frontend.onrender.com',credentials:true}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
